2fe0e1aa83acf5a62cb2e5e2a5e5aa499acea6a1
NullPatrol <NullPatrol@users.noreply.github.com>
2016-09-05 22:03:39 +0200

    Create testpage.php

---
+++ testpage.php
@@ -0,0 +1,8 @@
+<?php
+// Created by: CyberGuard
+// Contact: CyberGuard@hotmail.com
+// Keybase: https://keybase.io/CyberGuard
+// Twitter: @RealCyberGuard
+require_once('firewall'); // this is how you add the firewall to your web page
+
+echo "Are you blocked, or nah? Follow me on twitter because I'm fabulous, not because I told you! @RealCyberGuard";
